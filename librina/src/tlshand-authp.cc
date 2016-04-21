acba2455ea5374786149388082e57c54023f179e
root <root@debian.com>
2016-04-21 13:36:01 +0200

    modified:   librina/src/tlshand-authp.cc

--- librina/src/tlshand-authp.cc
+++ librina/src/tlshand-authp.cc
@@ -753,7 +753,7 @@ int AuthTLSHandPolicySet::process_client_key_exchange_message(const cdap::CDAPMe
 
 	if(rsa_pkey == NULL) LOG_ERR("EVP_PKEY_get1_RSA: failed.");
 
-	if((res =  RSA_public_decrypt(pre_master_secret.length, pre_master_secret.data, pre_master_secret.data, rsa_pkey, RSA_NO_PADDING)) == -1){
+	if((res =  RSA_public_decrypt(pre_master_secret.length, pre_master_secret.data, pre_master_secret.data, rsa_pkey, RSA_PKCS1_OAEP_PADDING)) == -1){
 		LOG_ERR("Error decrypting pre-master secret");
 		ERR_load_crypto_strings();
 		ERR_error_string(ERR_get_error(), err);
@@ -836,7 +836,7 @@ int AuthTLSHandPolicySet::send_client_key_exchange(TLSHandSecurityContext * sc)
 	if(rsa_pkey == NULL) LOG_ERR("EVP_PKEY_get1_RSA: failed.");
 
 	int res = -1;
-	if((res = RSA_public_encrypt(pre_master_secret.length, pre_master_secret.data, pre_master_secret.data, rsa_pkey, RSA_NO_PADDING)) == -1)
+	if((res = RSA_public_encrypt(pre_master_secret.length, pre_master_secret.data, pre_master_secret.data, rsa_pkey, RSA_PKCS1_OAEP_PADDING)) == -1)
 		LOG_ERR("Error encrypting pre-master secret");
 
 
